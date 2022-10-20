8084f76c969b1ba372d4e811703da9a1ad2b3f35
DarkLord76865 <40371578+darklord76865@users.noreply.github.com>
2022-10-20 21:47:06 +0200

    Update build.py

--- build.py
+++ build.py
@@ -12,25 +12,26 @@ def main():
 	admin = False
 	key = "DarkLord76865"
 	name = f"Autofish_{datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S')}"
-	run_data_path = "run_data"
-	icon_path = "data\\fish_icon.ico"
-	upx_path = "data\\upx.exe"
-	result_path = "."
+	run_data_path = os.path.join(os.path.abspath("."), "run_data")
+	icon_path = os.path.join(os.path.abspath("."), "data\\fish_icon.ico")
+	upx_path = os.path.join(os.path.abspath("."), "data")
+	result_path = os.path.abspath(".")
 	work_path = "build"
 
 	while os.path.isdir(work_path):
 		work_path = f"build_{random.randint(1, 1_000_000_000)}"
+	work_path = os.path.join(os.path.abspath("."), work_path)
 
 	run_list = ['main.py',
 	            '--noconfirm',
-	            f'--icon "{icon_path}"',
-	            f'--add-data "{icon_path};{os.path.dirname(icon_path)}"',
-	            f'--upx-dir "{upx_path}"',
-	            f'--key "{key}"',
-	            f'--name "{name}"',
-	            f'--workpath "{work_path}"',
-	            f'--distpath "{result_path}"',
-	            f'--specpath "{work_path}"']
+	            '--icon', icon_path,
+	            '--add-data', f"{icon_path};{os.path.basename(os.path.dirname(icon_path))}",
+	            '--upx-dir', f"{upx_path}",
+	            '--key', f"{key}",
+	            '--name', f"{name}",
+	            '--workpath', f"{work_path}",
+	            '--distpath', f"{result_path}",
+	            '--specpath', f"{work_path}"]
 
 	if admin:
 		run_list.append("--uac-admin")
@@ -50,7 +51,7 @@ def main():
 
 	for file in os.listdir(run_data_path):
 		if os.path.isfile(os.path.join(run_data_path, file)):
-			run_list.append(f'--add-data "{os.path.join(run_data_path, file)};{run_data_path}"')
+			run_list.extend(('--add-data', f"{os.path.join(run_data_path, file)};{os.path.basename(run_data_path)}"))
 
 	PyInstaller.__main__.run(run_list)
 
